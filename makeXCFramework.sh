bb9643b99cb1fd9bba6fa488483d971f31dba76f
David Ferrero <david.ferrero@zion.com>
2025-01-13 22:22:23 -0700

    fix script to ensure Vendor directory is present before copy

--- makeXCFramework.sh
+++ makeXCFramework.sh
@@ -1,6 +1,8 @@
 rm -rf Vendor/*
 rm -rf archives/*
 rm -rf xcframeworks/*
+rmdir Vendor
+mkdir Vendor
 cp -a ../plCrashReporter/xcframeworks/CrashReporter.xcframework Vendor/
 xcodebuild archive -project Support/HockeySDK.xcodeproj -scheme "HockeySDK" -configuration "ReleaseCrashOnly" -destination "generic/platform=macOS" -archivePath "archives/HockeySDK"
 xcodebuild -create-xcframework -archive archives/HockeySDK.xcarchive -framework HockeySDK.framework -output xcframeworks/HockeySDK-macOS.xcframework
