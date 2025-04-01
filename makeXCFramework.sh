952655d346bee76cc73efc3e99320299700f1ab4
David Ferrero <ferrerod@users.noreply.github.com>
2025-04-01 11:39:53 -0600

    Link to CrashReporter, do not embed (#7)

--- makeXCFramework.sh
+++ makeXCFramework.sh
@@ -6,6 +6,6 @@ rm -rf archives/*
 rm -rf xcframeworks/*
 rmdir Vendor
 mkdir Vendor
-cp -a ../plCrashReporter/xcframeworks/CrashReporter.xcframework Vendor/
+ditto ../plCrashReporter/xcframeworks/CrashReporter.xcframework Vendor/CrashReporter.xcframework
 xcodebuild archive -project Support/HockeySDK.xcodeproj -scheme "HockeySDK" -configuration "ReleaseCrashOnly" -destination "generic/platform=macOS" -archivePath "archives/HockeySDK"
 xcodebuild -create-xcframework -archive archives/HockeySDK.xcarchive -framework HockeySDK.framework -output xcframeworks/HockeySDK-macOS.xcframework
